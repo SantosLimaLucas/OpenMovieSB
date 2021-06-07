cf7b61cc38b9a41a31808c016bd5df652aff270a
SantosLimaLucas <lucaslimasantos88@hotmail.com>
2021-06-07 00:42:25 -0300

    Implentação de Cadastro de Usuários no Firebase

--- app/src/main/java/com/example/openmoviesb/FormCadastro.kt
+++ app/src/main/java/com/example/openmoviesb/FormCadastro.kt
@@ -2,7 +2,9 @@ package com.example.openmoviesb
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
 import com.example.openmoviesb.databinding.ActivityFormCadastroBinding
+import com.google.firebase.auth.FirebaseAuth
 
 class FormCadastro : AppCompatActivity() {
 
@@ -14,7 +16,41 @@ class FormCadastro : AppCompatActivity() {
         setContentView(binding.root)
 
         supportActionBar!!.hide()
+        Toolbar()
 
+        binding.btCadastrar.setOnClickListener {
+
+            val email = binding.editEmail.text.toString()
+            val senha = binding.editSenha.text.toString()
+            val mensagem_erro = binding.mensagemErro
+
+            if (email.isEmpty() || senha.isEmpty()){
+                mensagem_erro.setText("Preencha todos os campos")
+            }else{
+                CadastrarUsuario()
+            }
+        }
+
+    }
+
+    private fun CadastrarUsuario(){
+        val email = binding.editEmail.text.toString()
+        val senha = binding.editSenha.text.toString()
+        val mensagem_erro = binding.mensagemErro
+
+        FirebaseAuth.getInstance().createUserWithEmailAndPassword(email, senha).addOnCompleteListener {
+            if (it.isSuccessful){
+                Toast.makeText(this, "Usuário cadastrado com sucesso!", Toast.LENGTH_SHORT).show()
+                binding.editEmail.setText("")
+                binding.editSenha.setText("")
+                mensagem_erro.setText("")
+            }
+        }.addOnFailureListener {
+            mensagem_erro.setText(it.message)
+        }
+    }
+
+    private fun Toolbar(){
         val toolbar = binding.toolbarCadastro
         toolbar.setBackgroundColor(getColor(R.color.white))
         toolbar.setNavigationIcon(getDrawable(R.drawable.openmoviesb))
